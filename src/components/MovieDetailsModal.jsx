c9970dd6cffb8f08a4f071662fc0069594f74c78
lsarker2024 <lsarker2024@gmail.com>
2026-09-17 23:29:43 +0600

    Modal of Movie Details is done

---
+++ src/components/MovieDetailsModal.jsx
@@ -0,0 +1,54 @@
+
+import { useEffect, useState } from "react";
+const MovieDetailsModal = ({movieId, onClose}) => {
+    
+    const [movieData, setMovieData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        fetch('https://api.tvmaze.com/shows/'+movieId)
+        .then((res2)=>res2.json())
+        .then((data2)=>setMovieData(data2))
+        .finally(()=>{setIsLoading(false)});
+    },[]);
+        
+    if(isLoading){
+        return (
+                    <div className="text-amber-50 text-2xl text-center xl:absolute xl:top-2/6 xl:left-1/3 w-2xs">
+                        Loading...
+                    </div>
+                );
+    }
+    
+    return(
+        <div className="fixed inset-0 flex justify-center items-center">
+            <div className="h-[800px] w-[400px] bg-gray-100 shadow-2xl rounded-2xl">
+                <article data-movie="Movie Title One Action 2026" className="overflow-hidden rounded-[18px] border border-white/10 bg-[#101521] transition hover:-translate-y-1 hover:border-white/20 relative">
+                    <button type="button" 
+                        className="absolute right-3 top-3  pr-4  rounded-full text-white hover:text-amber-800 drop-shadow-black"
+                        onClick={onClose}>✕</button>
+                    <div className="absolute left-3 top-3 rounded-full g-black/75 px-2.5 py-1 text-base font-bold backdrop-blur-sm">
+                                        ⭐  <span>•Rating Score•</span> {movieData.rating.average} 
+                    </div>
+                    <div className="grid aspect-[2/3] place-items-center bg-[radial-gradient(circle_at_50%_25%,rgba(255,183,3,.25),transparent_22%),linear-gradient(145deg,#202a40,#17101f)] text-5xl">
+                            <img src={movieData.image.original} alt="" />
+                    </div>
+                    <div className="p-4">
+                            <h2 className="text-xl font-semibold">{movieData.name}</h2>
+                            <span className="text-base text-slate-500">📅 <span>•Release Date•</span> {movieData.premiered}</span>
+                    </div>
+                    <div className="p-4">
+                        <h2 className="text-xl">Overview</h2>
+                        Duration: {movieData.averageRuntime} minutes <br />
+                        Language: {movieData.language} <br />
+                       
+                        {movieData.summary.replace(/<\/?[^>]+(>|$)/g, "")}
+                        
+                    </div>
+                </article>
+                
+            </div>
+        </div>
+    );
+}
+export default MovieDetailsModal;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
+import MovieDetailsModal from "./MovieDetailsModal";
 
 const MovieList = () =>{
     const [movieData, setMovieData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const[movieId, setMovieId] = useState(null);
+    
+    
+    
     useEffect(()=>{
         fetch('https://api.tvmaze.com/shows')
         .then((res)=>res.json())
@@ -11,30 +16,40 @@ const MovieList = () =>{
     },[]);
 
     if(isLoading){
-        return <p className="text-amber-50 text-2xl text-center">Loading...</p>
+        return (
+                <div className="text-amber-50 text-2xl text-center xl:absolute xl:top-2/6 xl:left-1/3 w-2xs">
+                    Loading...
+                </div>
+            );
     }
+    if (movieId != null){
+        //console.log(movieId);
+        return(
+            <MovieDetailsModal movieId={movieId} onClose={()=>setMovieId(null)}/>
+        );
+    } 
 
     return(
             
         movieData.map((movie)=>(
             <article data-movie="Movie Title One Action 2026" className="overflow-hidden rounded-[18px] border border-white/10 bg-[#101521] transition hover:-translate-y-1 hover:border-white/20 relative">
                 <div className="absolute left-3 top-3 rounded-full g-black/75 px-2.5 py-1 text-base font-bold backdrop-blur-sm">
-                                ⭐  Rating: {movie.rating.average}
+                                ⭐  <span>•</span> {movie.rating.average}
                 </div>
                 <div className="grid aspect-[2/3] place-items-center bg-[radial-gradient(circle_at_50%_25%,rgba(255,183,3,.25),transparent_22%),linear-gradient(145deg,#202a40,#17101f)] text-5xl">
                     <img src={movie.image.medium} alt="" />
                 </div>
                 <div className="p-4">
                     <h2 className="text-xl font-semibold">{movie.name}</h2>
-                    <span className="text-base text-slate-500">📅 Release: {movie.premiered}</span>
+                    <span className="text-base text-slate-500">📅 <span>•</span> {movie.premiered}</span>
                 </div>
                 <div className="p-4">
-                    <a
-                    href="/"
+                    <button
+                    onClick={()=>setMovieId(movie.id)}
                     className="inline-flex min-h-10 w-full items-center justify-center rounded-lg border border-amber-400/40 bg-amber-400/10 px-4 text-xs font-bold text-amber-300 transition hover:bg-amber-400 hover:text-slate-950"
                     >
                     See Details
-                    </a>
+                    </button>
                 </div>
             </article>
         ))
