98b9b6fd79944853eb1898cafaabc130822a3f19
lsarker2024 <lsarker2024@gmail.com>
2026-09-18 16:19:05 +0600

    Small Adjustmet is Done for Release ... Finally

--- src/components/MovieDetailsModal.jsx
+++ src/components/MovieDetailsModal.jsx
@@ -21,13 +21,13 @@ const MovieDetailsModal = ({movieId, onClose}) => {
     }
     
     return(
-        <div className="fixed inset-0 flex justify-center items-center">
-            <div className="h-[720px] w-[400px] bg-gray-100 shadow-2xl rounded-2xl">
+        <div className="fixed inset-0 flex justify-center items-center z-999">
+            <div className="w-[400px] bg-gray-100 shadow-2xl rounded-2xl">
                 <article data-movie="Movie Title One Action 2026" className="overflow-hidden rounded-[18px] border border-white/10 bg-[#101521] transition hover:-translate-y-1 hover:border-white/20 relative">
                     <button type="button" 
-                        className="absolute right-3 top-3  pr-4  rounded-full text-white hover:text-amber-800 drop-shadow-black"
+                        className="absolute right-3 top-3  pr-4  rounded-full text-white hover:text-amber-800 drop-shadow-black z-9999"
                         onClick={onClose}>✕</button>
-                    <div className="absolute left-3 top-3 rounded-full g-black/75 px-2.5 py-1 text-base font-bold backdrop-blur-sm">
+                    <div className="absolute left-3 top-3 rounded-full g-black/75 px-2.5 py-1 text-base font-bold backdrop-blur-sm z-9999">
                                         ⭐  <span>•Rating Score•</span> {movieData.rating.average} 
                     </div>
                     <div className="grid aspect-[2/3] place-items-center bg-[radial-gradient(circle_at_50%_25%,rgba(255,183,3,.25),transparent_22%),linear-gradient(145deg,#202a40,#17101f)] text-5xl">
--- src/layouts/FooterLayout.jsx
+++ src/layouts/FooterLayout.jsx
@@ -2,7 +2,7 @@ const FooterLayout = () =>{
     return(
         <footer className="bg-[#070910] border-t border-white/10">
             <div className="mx-auto min-h-[120px] max-w-[1180px] px-4 text-center sm:min-h-[86px]">
-                <p className="text-xl text-slate-500 max-sm:text-lg">
+                <p className="py-5 text-xl text-slate-500 max-sm:text-lg">
                     © 2026 JossMovies. All rights reserved.
                 </p>
             </div>
